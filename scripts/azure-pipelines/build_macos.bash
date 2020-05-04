9269deb8aff4b48dfbaf06a2c3390957af9dfb81
yossizap <yossizap@users.noreply.github.com>
2020-05-04 21:09:30 +0300

    Updated travis and azure CI

--- scripts/azure-pipelines/build_macos.bash
+++ scripts/azure-pipelines/build_macos.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 #
-# Copyright 2019 The Mumble Developers. All rights reserved.
+# Copyright 2019-2020 The Mumble Developers. All rights reserved.
 # Use of this source code is governed by a BSD-style license
 # that can be found in the LICENSE file at the root of the
 # Mumble source tree or at <https://www.mumble.info/LICENSE>.
@@ -11,4 +11,14 @@ export PATH=$PATH:/usr/local/opt/qt5/bin:/usr/local/bin
 export MUMBLE_PREFIX=/usr/local
 export MUMBLE_ICE_PREFIX=/usr/local/opt/ice
 
-qmake -recursive CONFIG+="release tests warnings-as-errors" && make -j $(sysctl -n hw.ncpu) && make check
+ver=$(python scripts/mumble-version.py)
+
+qmake -recursive CONFIG+="release tests warnings-as-errors" DEFINES+="MUMBLE_VERSION=${ver}"
+
+make -j $(sysctl -n hw.ncpu)
+make check
+
+# Build installer
+./macx/scripts/osxdist.py --version=${ver}
+
+mv release/*.dmg ${BUILD_ARTIFACTSTAGINGDIRECTORY}
--- scripts/travis-ci/before_install.bash
+++ scripts/travis-ci/before_install.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 #
-# Copyright 2005-2019 The Mumble Developers. All rights reserved.
+# Copyright 2005-2020 The Mumble Developers. All rights reserved.
 # Use of this source code is governed by a BSD-style license
 # that can be found in the LICENSE file at the root of the
 # Mumble source tree or at <https://www.mumble.info/LICENSE>.
@@ -15,23 +15,23 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
 	# See mumble-voip/mumble#3312 for more information.
 	echo 'Acquire::http::Timeout "120";' | sudo tee /etc/apt/apt.conf.d/99zzztimeout
 
-	if [ "${MUMBLE_QT}" == "qt4" ] && [ "${MUMBLE_HOST}" == "x86_64-linux-gnu" ]; then
+	if [ "${MUMBLE_HOST}" == "x86_64-linux-gnu" ] || [ "${MUMBLE_HOST}" == "aarch64-linux-gnu" ]; then
 		sudo apt-get -qq update
-		sudo apt-get build-dep -qq mumble
-		sudo apt-get install libjack-jackd2-dev
-	elif [ "${MUMBLE_QT}" == "qt5" ] && [ "${MUMBLE_HOST}" == "x86_64-linux-gnu" ]; then
-		sudo apt-get -qq update
-		sudo apt-get build-dep -qq mumble
-		sudo apt-get install libqt5sql5 libqt5sql5-sqlite qt5-default qttools5-dev qttools5-dev-tools qtbase5-dev qtbase5-dev-tools qttranslations5-l10n libqt5svg5-dev
-		sudo apt-get install libjack-jackd2-dev
-	elif [ "${MUMBLE_QT}" == "qt5" ] && [ "${MUMBLE_HOST}" == "i686-w64-mingw32" ]; then
+		sudo apt-get -y install build-essential pkg-config qt5-default qttools5-dev-tools qttranslations5-l10n \
+                                libqt5svg5-dev libboost-dev libssl-dev libprotobuf-dev protobuf-compiler \
+                                libcap-dev libxi-dev \
+                                libasound2-dev libpulse-dev \
+                                libogg-dev libsndfile1-dev libspeechd-dev \
+                                libavahi-compat-libdnssd-dev libzeroc-ice-dev libg15daemon-client-dev \
+                                libgrpc++-dev libprotoc-dev protobuf-compiler-grpc
+	elif [ "${MUMBLE_HOST}" == "i686-w64-mingw32" ]; then
 		sudo dpkg --add-architecture i386
 		sudo apt-get -qq update
-		echo "deb https://dl.mumble.info/mirror/mirror.mxe.cc/repos/apt xenial main" | sudo tee /etc/apt/sources.list.d/mxe.list
+		echo "deb https://dl.mumble.info/mirror/pkg.mxe.cc/repos/apt bionic main" | sudo tee /etc/apt/sources.list.d/mxe.list
 		sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 86B72ED9
 		sudo apt-get -qq update
 		sudo apt-get install \
-			wine \
+			wine32-development wine-development \
 			mxe-${MUMBLE_HOST_DEB}.static-qtbase \
 			mxe-${MUMBLE_HOST_DEB}.static-qtsvg \
 			mxe-${MUMBLE_HOST_DEB}.static-qttools \
@@ -43,14 +43,14 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
 			mxe-${MUMBLE_HOST_DEB}.static-ogg \
 			mxe-${MUMBLE_HOST_DEB}.static-vorbis \
 			mxe-${MUMBLE_HOST_DEB}.static-libsndfile
-	elif [ "${MUMBLE_QT}" == "qt5" ] && [ "${MUMBLE_HOST}" == "x86_64-w64-mingw32" ]; then
+	elif [ "${MUMBLE_HOST}" == "x86_64-w64-mingw32" ]; then
 		sudo dpkg --add-architecture i386
 		sudo apt-get -qq update
-		echo "deb https://dl.mumble.info/mirror/mirror.mxe.cc/repos/apt xenial main" | sudo tee /etc/apt/sources.list.d/mxe.list
+		echo "deb https://dl.mumble.info/mirror/pkg.mxe.cc/repos/apt bionic main" | sudo tee /etc/apt/sources.list.d/mxe.list
 		sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 86B72ED9
 		sudo apt-get -qq update
 		sudo apt-get install \
-			wine \
+			wine64-development wine-development \
 			mxe-${MUMBLE_HOST_DEB}.static-qtbase \
 			mxe-${MUMBLE_HOST_DEB}.static-qtsvg \
 			mxe-${MUMBLE_HOST_DEB}.static-qttools \
@@ -65,18 +65,6 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
 	else
 		exit 1
 	fi
-elif [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-	# We install the protobuf package via brew,
-	# which depends on "python@2".
-	#
-	# The build image upgraded the installed "python",
-	# and now "python@2" conflicts with it when trying
-	# to create symlinks.
-	#
-	# We don’t use the symlinked "python" installed
-	# by default in the image, so we unlink it to allow
-	# the "python@2" package to be installed without conflict.
-	brew update && brew unlink python && brew install qt5 libogg libvorbis flac libsndfile protobuf openssl ice
 else
 	exit 1
 fi
